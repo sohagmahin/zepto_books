d5c574f34c9633ede74e3940b417ed8bb21e098e
sohagmahin <sohagmahin@gmail.com>
2025-04-06 16:44:01 +0600

    add BookList component with dynamic book fetching and pagination

--- src/components/book/book-list.jsx
+++ src/components/book/book-list.jsx
@@ -1,50 +1,76 @@
+import { useEffect, useState } from "react";
 import BookCard from "./book-card";
+import { Button } from "../ui/button";
 
-const books = [
-  {
-    id: 1,
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    image:
-      "https://www.gutenberg.org/cache/epub/26184/pg26184.cover.medium.jpg",
-  },
-  {
-    id: 1,
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    image:
-      "https://www.gutenberg.org/cache/epub/26184/pg26184.cover.medium.jpg",
-  },
-  {
-    id: 1,
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    image:
-      "https://www.gutenberg.org/cache/epub/26184/pg26184.cover.medium.jpg",
-  },
-  {
-    id: 1,
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    image:
-      "https://www.gutenberg.org/cache/epub/26184/pg26184.cover.medium.jpg",
-  },
-  {
-    id: 1,
-    title: "The Great Gatsby",
-    author: "F. Scott Fitzgerald",
-    image:
-      "https://www.gutenberg.org/cache/epub/26184/pg26184.cover.medium.jpg",
-  },
-];
 const BookList = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const [url, setUrl] = useState("https://gutendex.com/books?page=1");
+  const [nextPage, setNextPage] = useState(false);
+  const [previousPage, setPreviousPage] = useState(false);
+
+  useEffect(() => {
+    console.log(url);
+    setLoading(true);
+    setBooks([]);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        // parse the data to get the books
+        const books = data.results.map((book) => ({
+          id: book?.id,
+          title: book?.title,
+          author: book?.authors[0]?.name,
+          image: book?.formats["image/jpeg"],
+        }));
+
+        setBooks(books);
+        setNextPage(data?.next);
+        setPreviousPage(data?.previous);
+        setLoading(false);
+      });
+  }, [url]);
+
+  console.log(nextPage);
+
   return (
-    <div className="p-14 flex justify-center">
+    <div className="p-14 flex justify-center items-center flex-col gap-4">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        {/* Loading  */}
+
+        {loading && <div> Loading .....</div>}
+
         {books.map((book) => (
           <BookCard key={book.id} book={book} />
         ))}
+
+        {/* No books found */}
+        {!loading && books.length === 0 && <div>No books found</div>}
       </div>
+
+      {/* pagination */}
+      {!loading && (
+        <div
+          className="flex justify-center gap-4"
+          onClick={() => setUrl(previousPage)}
+        >
+          {previousPage && (
+            <Button className="bg-blue-500 text-white p-2 rounded-md">
+              Previous Page
+            </Button>
+          )}
+
+          {nextPage && (
+            <Button
+              className="bg-blue-500 text-white p-2 rounded-md"
+              onClick={() => setUrl(nextPage)}
+            >
+              Next Page
+            </Button>
+          )}
+        </div>
+      )}
     </div>
   );
 };
